384f89f8362aa04800223d4271a29a09a16253d5
srikanthpragada <srikanthpragada@gmail.com>
2025-08-14 19:59:01 +0530

    14-AUG

---
+++ hello.c
@@ -0,0 +1,9 @@
+// Program to display hello world
+// Date : 14-AUG-2025
+
+#include <stdio.h>
+
+void main()
+{
+  printf("Hello World!");
+}
---
+++ name.c
@@ -0,0 +1,10 @@
+// Program to print my name and mobile number
+// Date : 14-AUG-2025
+
+#include <stdio.h>
+
+void main()
+{
+  printf("Srikanth Technologies\n");
+  printf("9059057000");
+}
